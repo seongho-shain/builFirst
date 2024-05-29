a5646466df3e876205bae9875529bfac04c48e8c
seongho-shain <shain1912@gmail.com>
2024-05-29 19:27:28 +0900

    button 테스트

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace builFirst
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Hihi");
+        }
     }
 }
